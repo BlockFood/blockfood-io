6366d8be00a6742640d37b74d4ff566fcf7091e1
Conrad <conrad@blockfood.io>
2017-12-06 16:01:48 +0100

    publish to GitHub

---
+++ README.md
@@ -0,0 +1,23 @@
+# BlockFood.io
+
+This website is the central point of information regarding the BlockFood project.
+
+It is accessible on [BlockFood.io](https://blockfood.io).
+
+## Dev
+
+### Prerequisites
+
+- Have a local http server
+
+```bash
+npm i -g live-server
+```
+
+### Launch
+
+From the project root, launch your local http server (live-server for instance):
+
+```bash
+live-server
+```
---
+++ blockfood.io/js/main.js
@@ -0,0 +1,172 @@
+;(function () {
+	
+	'use strict';
+
+
+
+	// iPad and iPod detection	
+	var isiPad = function(){
+		return (navigator.platform.indexOf("iPad") != -1);
+	};
+
+	var isiPhone = function(){
+	    return (
+			(navigator.platform.indexOf("iPhone") != -1) || 
+			(navigator.platform.indexOf("iPod") != -1)
+	    );
+	};
+
+	// Go to next section
+	var gotToNextSection = function(){
+		var el = $('.bfio-learn-more'),
+			w = el.width(),
+			divide = -w/2;
+		el.css('margin-left', divide);
+	};
+
+	// Loading page
+	var loaderPage = function() {
+		$(".bfio-loader").fadeOut("slow");
+	};
+
+
+
+	// Scroll Next
+	var ScrollNext = function() {
+		$('body').on('click', '.scroll-btn', function(e){
+			e.preventDefault();
+
+			$('html, body').animate({
+				scrollTop: $( $(this).closest('[data-next="yes"]').next()).offset().top
+			}, 1000, 'easeInOutExpo');
+			return false;
+		});
+	};
+
+	// Click outside of offcanvass
+	var mobileMenuOutsideClick = function() {
+
+		$(document).click(function (e) {
+	    var container = $("#bfio-offcanvas, .js-bfio-nav-toggle");
+	    if (!container.is(e.target) && container.has(e.target).length === 0) {
+
+	    	if ( $('body').hasClass('offcanvas-visible') ) {
+
+    			$('body').removeClass('offcanvas-visible');
+    			$('.js-bfio-nav-toggle').removeClass('active');
+				
+	    	}
+	    }
+		});
+
+	};
+
+
+	// Offcanvas
+	var offcanvasMenu = function() {
+		$('body').prepend('<div id="bfio-offcanvas" />');
+		$('#bfio-offcanvas').prepend('<ul id="bfio-side-links">');
+		$('body').prepend('<a href="#" class="js-bfio-nav-toggle bfio-nav-toggle"><i></i></a>');
+
+		$('.left-menu li, .right-menu li').each(function(){
+
+			var $this = $(this);
+
+			$('#bfio-offcanvas ul').append($this.clone());
+
+		});
+	};
+
+	// Burger Menu
+	var burgerMenu = function() {
+
+		$('body').on('click', '.js-bfio-nav-toggle', function(event){
+			var $this = $(this);
+
+			$('body').toggleClass('bfio-overflow offcanvas-visible');
+			$this.toggleClass('active');
+			event.preventDefault();
+
+		});
+
+		$(window).resize(function() {
+			if ( $('body').hasClass('offcanvas-visible') ) {
+		   	$('body').removeClass('offcanvas-visible');
+		   	$('.js-bfio-nav-toggle').removeClass('active');
+		   }
+		});
+
+		$(window).scroll(function(){
+			if ( $('body').hasClass('offcanvas-visible') ) {
+		   	$('body').removeClass('offcanvas-visible');
+		   	$('.js-bfio-nav-toggle').removeClass('active');
+		   }
+		});
+
+	};
+
+
+	var goToTop = function() {
+
+		$('.js-gotop').on('click', function(event){
+			
+			event.preventDefault();
+
+			$('html, body').animate({
+				scrollTop: $('html').offset().top
+			}, 500);
+			
+			return false;
+		});
+	
+	};
+
+
+
+	// Animations
+
+	var contentWayPoint = function() {
+		var i = 0;
+		$('.animate-box').waypoint( function( direction ) {
+
+			if( direction === 'down' && !$(this.element).hasClass('animated') ) {
+				
+				i++;
+
+				$(this.element).addClass('item-animate');
+				setTimeout(function(){
+
+					$('body .animate-box.item-animate').each(function(k){
+						var el = $(this);
+						setTimeout( function () {
+							el.addClass('fadeInUp animated');
+							el.removeClass('item-animate');
+						},  k * 200, 'easeInOutExpo' );
+					});
+					
+				}, 100);
+				
+			}
+
+		} , { offset: '95%' } );
+	};
+	
+	
+
+	// Document on load.
+	$(function(){
+		gotToNextSection();
+		loaderPage();
+		ScrollNext();
+		mobileMenuOutsideClick();
+		offcanvasMenu();
+		burgerMenu();
+		goToTop();
+
+		// Animate
+		// contentWayPoint();
+
+	});
+
+
+}());
